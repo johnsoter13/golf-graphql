7d6f6e10f46bf40dd750ff45ef89bfd57a962da0
John Soter <john.soter@outlook.com>
2026-03-12 22:14:13 -0400

    fix: updates to api

---
+++ index.js
@@ -0,0 +1,17 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import { resolvers } from "./resolvers.js";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const port = Number(process.env.PORT) || 4000;
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port },
+});
+
+console.log(`Server ready at ${url}`);
